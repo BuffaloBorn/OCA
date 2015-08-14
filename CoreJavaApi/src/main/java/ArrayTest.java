802a1ed17f83ccebcbf4595e5377a375cfd59c16
vitaly_sereda <tifoha@gmail.com>
2015-08-14 23:05:55 +0300

    jkgj

--- CoreJavaApi/src/main/java/ArrayTest.java
+++ CoreJavaApi/src/main/java/ArrayTest.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by vitaly on 13.08.15.
  */
@@ -6,5 +9,11 @@ public class ArrayTest {
         String[] strings = {"1", "2", "3"};
         Object[] objects = strings;
 //        objects[0] = new Object(); //ArrayStoreException
+
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(3);
+        list.add(2);
+
     }
 }
