9b0c993fe65e1fb6e6e48966bac2418270f46328
Carl McGruder <carl.mcgruder@gmail.com>
2016-11-16 18:55:26 -0500

    fixed typo in comments

--- Ch5_ClassDesign/src/main/java/AbstractClasses.java
+++ Ch5_ClassDesign/src/main/java/AbstractClasses.java
@@ -1,14 +1,14 @@
 /**
  * Created by vitaly on 20.08.15.
  */
-//ABSTRACT CLASS DEFI NITION RULES:
+//ABSTRACT CLASS DEFINITION RULES:
 //        1.    Abstract classes cannot be instantiated directly.
 //        2.    Abstract classes may be defined with any number, including zero, of abstract and non-abstract methods.
 //        3.    Abstract classes may not be marked as private or final.
 //        4.    An abstract class that extends another abstract class inherits all of its abstract methods as its own abstract methods.
 //        5.    The first concrete class that extends an abstract class must provide an implementation for all of the inherited abstract methods.
 
-//ABSTRACT METHOD DEFI NITION RULES:
+//ABSTRACT METHOD DEFINITION RULES:
 //        1.  Abstract methods may only be defined in abstract classes.
 //        2.  Abstract methods may not be declared private or final.
 //        3.  Abstract methods must not provide a method body/implementation in the abstract class for which is it declared.
