46db6c62e53528f076295f03edab65a91358fdad
Carl McGruder <carl.mcgruder@gmail.com>
2016-12-16 17:23:26 -0500

    added some in line comments to this class

--- Ch5_ClassDesign/src/main/java/review/questions/q20/Bird.java
+++ Ch5_ClassDesign/src/main/java/review/questions/q20/Bird.java
@@ -8,7 +8,20 @@ public class Bird {
 	}
 
 	public static void main(String[] args) {
-		Bird bird = new Pelican();
+		//Bird bird = new Pelican();
+		Pelican bird = new Pelican(); // testing out the alternative portion
 		bird.fly();
 	}
 }
+
+/**
+ * The code compiles and runs without issue, so options C, D, and E are incorrect.
+The trick here is that the method fly() is marked as private in the parent class Bird,
+which means it may only be hidden, not overridden. With hidden methods, the specific
+method used depends on where it is referenced. Since it is referenced within the Bird
+class, the method declared on line 2 was used, and option A is correct. Alternatively,
+if the method was referenced within the Pelican class, or if the method in the parent
+class was marked as protected and overridden in the subclass, then the method on line
+9 would have been used. 
+ */
+ 
