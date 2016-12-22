97a05ee9eda16c404d1d968a83a762967916ab68
Carl McGruder <carl.mcgruder@gmail.com>
2016-12-21 22:06:22 -0500

    added weight property to test lamba expression with a new statement that doesn't contain Panda object and let the compile figure out what type it is

--- Exams/src/q20161219023840/Panda.java
+++ Exams/src/q20161219023840/Panda.java
@@ -4,11 +4,15 @@ import java.util.function.Predicate;
 
 public class Panda {
 	int age; 
+	int weight; 
 
 	public static void main(String[] args) {
 		Panda p1 = new Panda();
 		p1.age = 7;
+		p1.weight = 190;
 		check(p1, (Panda p) -> p.age < 5);
+		check(p1,  w -> w.weight > 180);
+		//check(p1,  o -> o.age < 5);
 		//check(p1, Panda p -> p.age < 5);
 	}
 	
