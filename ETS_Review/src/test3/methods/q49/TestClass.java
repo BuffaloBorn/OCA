3ae60ded885a492d4518a375d20549e276b5b5a2
Carl McGruder <carl.mcgruder@gmail.com>
2017-01-17 21:17:39 -0500

    still trying to understand why this compiles with no errors. Added another instance variable to see what would happen using a class/static reference class variable

--- ETS_Review/src/test3/methods/q49/TestClass.java
+++ ETS_Review/src/test3/methods/q49/TestClass.java
@@ -3,13 +3,17 @@ package test3.methods.q49;
 class TestClass{ 
 	static TestClass ref; 
 	String[] arguments; 
+	boolean switchHolder; 
 	
 	public static void main(String args[]){ 
 		ref = new TestClass(); 
+		System.out.println(ref.switchHolder);
 		ref.func(args);
+		System.out.println(ref.switchHolder);
 	 } 
 	
 	public void func(String[] args){ 
 		ref.arguments = args;
+		ref.switchHolder = true;
 	} 
 } 
